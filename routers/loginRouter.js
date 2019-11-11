2034ab51f5577683d0a843a7930f4cc533b0d10a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-11 17:20:31 -0500

    adds authentication to get/users

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -9,7 +9,7 @@ router.post('/', (req, res) => {
     .first()
     .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-            res.status(200).json({ message: `Welcome ${user.username}!` });
+            res.status(200).json({ id: user.id, username: user.username });
         }
         else {
             res.status(401).json({ message: 'Invalid Credentials' });
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,9 @@
 const Users = require('../data/helpers')
 const router = require('express').Router()
+const withAuth = require('../middleware/withAuth')
 
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     Users.find()
     .then(users => res.status(200).json(users))
     .catch(err => res.status(500).json({ error: 'Could not get users'}))
