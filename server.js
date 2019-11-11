13c88c2bee56d0f264e879c6ba8796deaae06fa7
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-11 15:27:29 -0500

    sets up server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api', (req, res) => {
+    res.json({ message: 'API running' })
+})
+
+module.exports = server;
