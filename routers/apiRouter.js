f4aa5e9294ba2d7bcb4068354f9035edfd376fb0
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-12 16:20:24 -0500

    successfully persists login, register, logout sessions

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const userRouter = require('./userRouter')
 const registerRouter = require('./registerRouter');
 const loginRouter = require('./loginRouter')
+const logoutRouter = require('./logoutRouter')
 
 router.use('/users', userRouter)
 router.use('/register', registerRouter)
 router.use('/login', loginRouter)
+router.use('/logout', logoutRouter)
 
 module.exports = router;
